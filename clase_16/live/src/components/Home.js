3f01b3df06d7dad3a266bfa307ee4f296f873b97
gonzalo_arribalzaga <gonzalo.arribalzaga@gmail.com>
2021-09-13 13:56:44 -0300

    cambio estilos clase en vivo 16

--- clase_16/live/src/components/Home.js
+++ clase_16/live/src/components/Home.js
@@ -9,6 +9,7 @@ function Home(props) {
           padding: "20px",
           textAlign: "center",
           border: "2px solid black",
+          justifyContent: "space-between"
         }}
       >
         <Link to="/">Home</Link>
@@ -16,6 +17,8 @@ function Home(props) {
         <Link to="/faq">Faq</Link>
         <Link to="/about">About</Link>
         <Link to="/article/127">Article 127</Link>
+        <Link to="/article/128">Article 128</Link>
+        <Link to="/article/129">Article 129</Link>
       </div>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <div
